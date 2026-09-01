14debd86e1541765f528a948c822dc615386d736
Francesco <francescosantiagovallegarcia@gmail.com>
2026-09-01 11:50:48 -0600

    CREATE

---
+++ Unidad2/01_condiciones.py
@@ -0,0 +1,7 @@
+#Todo valor diferentes de 0 dara True
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+print(bool(10000))
+print(bool(10.5))
+print(bool("A"))
---
+++ Unidad2/02_operadores.py
@@ -0,0 +1,6 @@
+operador=input("Ingrese la operación: ")
+o1=input("ingrese el numero1: ")
+o2=input("ingrese el numero2: ")
+resultado=o1+operador+o2
+resultado=eval(resultado)
+print(resultado)
